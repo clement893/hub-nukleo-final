5950ddc4f46a4bbb98a29e606a42d51b7cbaf7fd
clement893 <clement@nukleo.com>
2025-12-20 09:29:55 -0500

    feat: Ajouter un fichier d'index pour les composants de gestion

---
+++ apps/web/app/gestion/components/index.ts
@@ -0,0 +1,8 @@
+export { PageHeader } from "./PageHeader";
+export { EmployeeFilters } from "./EmployeeFilters";
+export { EmployeeTable, type Employee } from "./EmployeeTable";
+export { Pagination } from "./Pagination";
+export { EmployeeForm } from "./EmployeeForm";
+export { EmployeeModal } from "./EmployeeModal";
+export { EmployeeDeleteModal } from "./EmployeeDeleteModal";
+
